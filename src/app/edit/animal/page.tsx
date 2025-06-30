8f4fa144f7f3dd309ac91bd0f3514eb91262aecf
gmccannon <gwm13@uakron.edu>
2025-06-30 18:59:46 -0400

    Chnages file names

---
+++ src/app/edit/animal/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <p>
+      This is the edit pageXOffset.
+    </p>
+  )
+}
+
+export default Page
---
+++ src/app/remove/animal/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <p>
+      Remove page
+    </p>
+  )
+}
+
+export default Page
