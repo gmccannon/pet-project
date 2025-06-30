84baa86a3ac558f7eef1d3d96e4653103bb58d92
gmccannon <gwm13@uakron.edu>
2025-06-30 17:21:20 -0400

    Adds route files

---
+++ src/lib/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-white p-4 text-black">
+      <ul className="flex justify-around">
+        <li>
+          <Link href="/">Search Animals</Link>
+        </li>
+        <li>
+          <Link href="/add/animal">Add Animal</Link>
+        </li>
+        <li>
+          <Link href="/edit/animal">Edit Animal</Link>
+        </li>
+        <li>
+          <Link href="/remove/animal">Remove Animal</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
